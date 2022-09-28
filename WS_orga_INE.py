4c277bf0a8d97b752c187ba6a469fd5e5b19ee5c
1u1s4 <99617682+1u1s4@users.noreply.github.com>
2022-09-28 10:45:20 -0600

    conexionQ
    para comprobar si la pagina del INE esta en linea

--- WS_orga_INE.py
+++ WS_orga_INE.py
@@ -2,6 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
+def conexionQ() -> bool:
+    try:
+        URL = "https://www.ine.gob.gt/ine/institucion/organizacion/"
+        headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
+        r = requests.get(url=URL, headers=headers)
+        return True
+    except:
+        return False
 
 def junta_directiva(ruta: str):
     URL = "https://www.ine.gob.gt/ine/institucion/organizacion/"
