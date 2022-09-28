05e4d87485aedfcf7cb18b9c18b54ae649e374e6
1u1s4 <99617682+1u1s4@users.noreply.github.com>
2022-09-28 10:53:13 -0600

    Update WS_orga_INE.py

--- WS_orga_INE.py
+++ WS_orga_INE.py
@@ -6,7 +6,7 @@ def conexionQ() -> bool:
     try:
         URL = "https://www.ine.gob.gt/ine/institucion/organizacion/"
         headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
-        r = requests.get(url=URL, headers=headers)
+        requests.get(url=URL, headers=headers)
         return True
     except:
         return False
