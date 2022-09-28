c0076a22cb8f6843d987af2abf70a7cff4d22646
1u1s4 <99617682+1u1s4@users.noreply.github.com>
2022-09-28 07:21:11 -0600

    mas capitulos

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from xml.etree.ElementPath import prepare_self
 from reporteine import ReporteINE
 from time import time
 from datosipc import datosIPC
 
 if __name__ == "__main__":
     t = time()
-    datos = datosIPC()
+    datos = datosIPC(8, 2022)
     reporte = ReporteINE(
         nombre="Prueba_IPC"
     )
+# capitulo 2
     reporte.agregar_capitulo(
-        titulo="Variables que condicionan los precios internos"
+        titulo="Variables exógenas"
     )
     subcap_data = datos.indice_precio_alimentos()
     reporte.agregar_subcapitulo(
@@ -24,6 +24,7 @@ if __name__ == "__main__":
         data=subcap_data[0],
         opciones_grafico=dict(Q4Etiquetas=True)
     )
+
     subcap_data = datos.petroleo()
     reporte.agregar_subcapitulo(
         indice_capitulo=0,
@@ -97,6 +98,91 @@ if __name__ == "__main__":
         data=subcap_data[0],
         opciones_grafico={"precision":2}
     )
+# capitulo 3
+    reporte.agregar_capitulo(
+        titulo="Resultados del IPC"
+    )
+    subcap_data = datos.serie_IPC(0)
+    reporte.agregar_subcapitulo(
+        indice_capitulo=1,
+        titulo="Evolución del IPC",
+        titulo_grafico="IPC, base diciembre del 2010",
+        descripcion_grafico="República de Guatemala, Serie histórica 1 año, adimensional",
+        descripcion=subcap_data[1],
+        fuente="INE",
+        tipo_grafico="lineal",
+        data=subcap_data[0],
+        opciones_grafico={"precision":2, "Q4Etiquetas":True}
+    )
+    subcap_data = datos.serie_inflacion(0, 'interanual')
+    reporte.agregar_subcapitulo(
+        indice_capitulo=1,
+        titulo="Evolución del cambio anual del IPC",
+        titulo_grafico="Variación interanual del IPC",
+        descripcion_grafico="República de Guatemala, serie histórica, en porcentaje",
+        descripcion=subcap_data[1],
+        fuente="INE",
+        tipo_grafico="lineal",
+        data=subcap_data[0],
+        opciones_grafico={"precision":2, "Q4Etiquetas":True}
+    )
+    subcap_data = datos.serie_inflacion(0, 'acumulada')
+    reporte.agregar_subcapitulo(
+        indice_capitulo=1,
+        titulo="Evolución del cambio acumulado del IPC",
+        titulo_grafico="Variación acumulada del IPC",
+        descripcion_grafico="República de Guatemala, serie histórica, en porcentaje",
+        descripcion=subcap_data[1],
+        fuente="INE",
+        tipo_grafico="lineal",
+        data=subcap_data[0],
+        opciones_grafico={"precision":2, "Q4Etiquetas":True}
+    )
+    subcap_data = datos.serie_inflacion(0, 'intermensual')
+    reporte.agregar_subcapitulo(
+        indice_capitulo=1,
+        titulo="Evolución del cambio mensual del IPC",
+        titulo_grafico="Variación intermensual del IPC",
+        descripcion_grafico="República de Guatemala, serie histórica, en porcentaje",
+        descripcion=subcap_data[1],
+        fuente="INE",
+        tipo_grafico="lineal",
+        data=subcap_data[0],
+        opciones_grafico={"precision":2, "Q4Etiquetas":True}
+    )
+    subcap_data = datos.serie_poder_adquisitivo(0)
+    reporte.agregar_subcapitulo(
+        indice_capitulo=1,
+        titulo="Valor del dinero",
+        titulo_grafico="Poder adquisitivo del quetzal",
+        descripcion_grafico="República de Guatemala, serie histórica mensual, en porcentaje",
+        descripcion=subcap_data[1],
+        fuente="INE",
+        tipo_grafico="lineal",
+        data=subcap_data[0],
+        opciones_grafico={"precision":2, "Q4Etiquetas":True}
+    )
+#capitulo 4
+    reporte.agregar_capitulo(
+        titulo="Serie histórica de índices a nivel nacional de todos los gastos básicos"
+    )
+    datos_gba = datos.series_Gba(0)
+    for Gba in datos_gba:
+        nombre = Gba[0]
+        datos = Gba[1]
+        desc = Gba[2]
+        reporte.agregar_subcapitulo(
+            indice_capitulo=2,
+            titulo=f"Evolución del IPC del gasto basico {nombre}",
+            titulo_grafico="IPC, base diciembre del 2010",
+            descripcion_grafico="República de Guatemala, Serie histórica 1 año, adimensional",
+            descripcion=desc,
+            fuente="INE",
+            tipo_grafico="lineal",
+            data=datos,
+            opciones_grafico={"precision":2, "Q4Etiquetas":True}
+        )
+
     reporte.crear_reporte()
     #reporte.compilar_reporte()
     tf = time()
