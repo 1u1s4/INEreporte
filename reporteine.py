c23322746686002caccdc915f28267b4567061f7
1u1s4 <99617682+1u1s4@users.noreply.github.com>
2023-02-03 21:57:23 -0600

    orden

--- reporteine.py
+++ reporteine.py
@@ -423,10 +423,10 @@ class ReporteINE:
             for capitulo in self.__data["capitulos"]:
                 if capitulo['anexo']:
                     self.escribir_capitulo(capitulo, f)
-            f.write("\\includepdf{plantilla/contraportada.pdf}\n")
             f.write("\\input{tex/glosario.tex}\n")
             f.write("\\INEchaptercarta{Principales fórmulas de cálculo}{}\n")
             f.write("\\input{tex/formulas.tex}\n")
+            f.write("\\includepdf{plantilla/contraportada.pdf}\n")
             f.write("\\end{document}\n")
             f.close()
             
