6846782fe08c496d524015c8ba93a2e69b927595
1u1s4 <3553751921101@ingenieria.usac.edu.gt>
2023-06-05 22:53:23 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='reporteine',
-    version='0.3.0',
+    version='0.3.1',
     author='Luis Alfredo Alvarado Rodríguez',
     description='Creador de reportes estilo INE.',
     long_description='',
