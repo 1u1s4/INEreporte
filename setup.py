00f6f83c01678e82df2253ee1442f6115611b46b
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2023-06-06 11:19:54 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='reporteine',
-    version='0.3.1',
+    version='0.3.2',
     author='Luis Alfredo Alvarado Rodríguez',
     description='Creador de reportes estilo INE.',
     long_description='',
     url='https://github.com/1u1s4/INE_LaTeX',
     keywords='development, setup, setuptools',
-    python_requires='>=3',
+    python_requires='>=3.9',
     packages=find_packages(),
     py_modules=['funcionesINE', 'reporteine', 'WS_orga_INE'],
     install_requires=[
@@ -17,7 +17,8 @@ setup(
         'requests',
         'bs4',
         'xlrd==2.0.1',
-        'openpyxl'
+        'openpyxl',
+        'html5lib'
     ],
     package_data={
         'reporteine': ['Fuentes/*', 'Plantilla/*', 'Direcciones/DCE/*', 'Direcciones/DIEC/*', 'Direcciones/DPIR/*'],
