7f600f79e5771a8ea9ed1e24b5b0ca6139b1dd15
1u1s4 <3553751921101@ingenieria.usac.edu.gt>
2023-06-10 15:35:59 -0600

    nuevo nombre de carpeta

--- reporteine/reporteine.py
+++ reporteine/reporteine.py
@@ -31,7 +31,8 @@ class ReporteINE:
         self.mes = mes
         self.direccion = direccion
         # hacer directorio para guardar documentos
-        marca_temporal = datetime.strftime(datetime.today(), "%d-%m-%Y_%H_%M_%S")
+        nombre_archivo = self.quitar_tildes(nombre_reporte).replace(" ", "_").lower()
+        marca_temporal = datetime.strftime(datetime.today(), "nombre_archivo_%d-%m-%Y_%H_%M_%S")
         parent_dir = pathlib.Path().resolve()
         self.__path = os.path.join(parent_dir, marca_temporal)
         # arbol de carpetas
