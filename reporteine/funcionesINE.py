19f58f311f96337266df9f0c5668fe2444cc1932
1u1s4 <3553751921101@ingenieria.usac.edu.gt>
2023-06-10 19:27:10 -0600

    Update funcionesINE.py

--- reporteine/funcionesINE.py
+++ reporteine/funcionesINE.py
@@ -120,3 +120,114 @@ class FuncionesINE:
             ruta_salida + f"/{nombre}.tex",
             g
         )
+
+    def tabla_LaTeX(
+        self,
+        datos,
+        ruta_salida,
+        nombre,
+        precision: int = 2,
+        tabla_inflacion: bool = False):
+        with open(os.path.join(ruta_salida, f"{nombre}.tex"), "w", encoding="utf-8") as f:
+            f.write("\\setlength{\\arrayrulewidth}{1.5pt}\n")
+            f.write("\\definecolor{Fcolor}{HTML}{e5e5fa}\n")
+            f.write("\\definecolor{Lcolor}{HTML}{4d80ff}\n")
+            # si es tabla de inflacion lleva esa cosa sobre los meses de "Inflacion interanual a"
+            if tabla_inflacion:
+                f.write("\\setlength{\\aboverulesep}{-1pt}\n")
+                f.write("\\setlength{\\belowrulesep}{0pt}\n")
+            alinacion = len(datos[0])*"c"
+            f.write("\\begin{tabular}{" + alinacion + "}\n")
+            f.write("\\arrayrulecolor{Lcolor} \hline\n")
+            # si es tabla de inflacion lleva esa cosa sobre los meses de "Inflacion interanual a"
+            if tabla_inflacion:
+                f.write("\\rowcolor{Fcolor} & \\multicolumn{2}{c}{\\textbf{Inflacion interanual a}}\\\\\n")
+                f.write("\\cmidrule[0.9pt]{2-3}\n")
+            f.write("\\rowcolor{Fcolor} ")
+            i = 0
+            for encabezado in datos[0]:
+                i += 1
+                f.write("\\textbf{" + encabezado + "}")
+                if i != len(datos[0]):
+                    f.write(" & ")
+                else:
+                    f.write("\\\\\n")
+            f.write("\\hline\n")
+            f.write("\\rowcolor{white} ")
+            for i in range(1, len(datos)):
+                j = 0
+                for celda in datos[i]:
+                    j += 1
+                    if type(celda) is str:
+                        f.write(f"{celda}")
+                    else:
+                        f.write(f"{float(celda):.{precision}f}")
+                    if j != len(datos[i]):
+                        f.write(" & ")
+                    else:
+                        f.write("\\\\\n")
+            f.write("\\hline\n")
+            f.write("\\end{tabular}\n")
+
+    '''
+    def export_to_longtable(
+            self,
+            df: pd.DataFrame,
+            filename: str,
+            ruta_salida: str,
+            header: bool=True,
+            decimals: int=2):
+        """
+        Exporta un dataframe a un longtable de LaTeX en un archivo.
+        
+        Parameters:
+        df (pandas.DataFrame): DataFrame a exportar.
+        filename (str): Nombre del archivo a guardar.
+        caption (str): Leyenda de la tabla.
+        column_format (str): Cadena que describe el formato de las columnas.
+        header (bool): Si True, muestra el encabezado de la tabla.
+        decimals (int): Cantidad de decimales para los números. Si el valor no es numérico, se muestra tal cual.
+        """
+        with open(f"{ruta_salida}/{filename}.tex", 'w', encoding="utf-8") as f:
+            f.write('\\begin{longtable}{' + 'c'*len(df.columns) + '}\n')
+            f.write('\\toprule\n')
+            
+            # Escribir el encabezado
+            if header:
+                f.write(' & '.join([col.replace('_', '\\_') for col in df.columns]))
+                f.write('\\\\\\midrule\n')
+            
+            f.write('\\endfirsthead\n')
+            f.write('\\multicolumn{' + str(len(df.columns)) + '}{c}{{\\bfseries \\tablename\\ \\thetable{} -- '
+                    'Continuación de la página anterior}}\\\\\n')
+            f.write('\\toprule\n')
+            if header:
+                f.write(' & '.join([col.replace('_', '\\_') for col in df.columns]))
+                f.write('\\\\\\midrule\n')
+            f.write('\\endhead\n')
+            f.write('\\midrule\n')
+            f.write('\\multicolumn{' + str(len(df.columns)) + '}{r}{{Continúa en la siguiente página}}\\\\\n')
+            f.write('\\endfoot\n')
+            f.write('\\bottomrule\n')
+            f.write('\\endlastfoot\n')
+            
+            # Escribir los datos
+            import re
+            for _, row in df.iterrows():
+                values = []
+                for value in row:
+                    if pd.isna(value):
+                        values.append('')
+                    elif isinstance(value, (int, float)):
+                        valor = round(value, decimals)
+                        if re.match(r"^[-]0\.0*$", str(valor)):
+                            valor = int(valor)
+                        valor = '{:.{}f}'.format(valor, decimals)
+                        values.append(valor)
+                    else:
+                        values.append(str(value))
+                f.write(' & '.join(values))
+                f.write('\\\\\n')
+            
+            f.write('\\end{longtable}\n')
+        '''
