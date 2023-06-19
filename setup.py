20de373833836d18f75163065df8c64a17080206
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2023-06-19 09:30:06 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='reporteine',
-    version='0.3.18',
+    version='0.3.19',
     author='Luis Alfredo Alvarado Rodríguez',
     description='Creador de reportes estilo INE.',
     long_description='',
